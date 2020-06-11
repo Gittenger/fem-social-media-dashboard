7228a5a3ef61411fff2c51d3c7557daf44eb9231
gittenger <john.pittenger86@gmail.com>
2020-06-11 14:16:24 -0400

    switch button styles updated

--- src/components/switch/switch.styles.jsx
+++ src/components/switch/switch.styles.jsx
@@ -2,23 +2,22 @@ import styled, { css } from "styled-components";
 
 const normalStyles = css`
   & > div:first-child {
-    transform: translateX(0);
-    background-color: darkgrey;
+    transform: translateX(0.2rem);
+    background-color: ${({ theme: { colors } }) => colors.backgroundGrad2};
   }
 `;
 
 const activeStyles = css`
   & > div:first-child {
-    transform: translateX(1rem);
-    background-color: dodgerblue;
+    transform: translateX(2rem);
   }
 `;
 
 export const SwitchContainer = styled.div`
-  background-color: lightgrey;
+  background: ${({ theme: { colors } }) => colors.toggle};
   border-radius: 1rem;
-  width: 2.3rem;
-  height: 1.2rem;
+  width: 3.9rem;
+  height: 1.9rem;
   display: flex;
   align-items: center;
   transform: scale(1);
@@ -32,8 +31,8 @@ export const SwitchContainer = styled.div`
 `;
 
 export const SwitchButton = styled.div`
-  height: 1.4rem;
-  width: 1.4rem;
+  height: 1.7rem;
+  width: 1.7rem;
   border-radius: 50%;
   transition: transform 0.5s;
 `;
--- src/style-utils/theme.js
+++ src/style-utils/theme.js
@@ -19,6 +19,8 @@ export const darkTheme = {
     toggle: "linear-gradient(to right, #378fe6, #3eda82)",
     background:
       "linear-gradient(to bottom, #1f212e, #1f212e 25rem, #1e202a 1%, #1e202a)",
+    backgroundGrad1: "#1f212e",
+    backgroundGrad2: "#1e202a",
     cardBg: "#252a41",
     textSub: "#8b97c6",
     textMain: "#ffffff",
@@ -34,6 +36,8 @@ export const lightTheme = {
     toggle: "#aeb3cb",
     background:
       "linear-gradient(to bottom, #f5f7ff, #f5f7ff 25rem, #ffffff 1%, #ffffff)",
+    backgroundGrad1: "#f5f7ff",
+    backgroundGrad2: "#ffffff",
     cardBg: "#f0f2fa",
     textSub: "#63687e",
     textMain: "#1e202a",
